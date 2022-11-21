7d5d4a20f0c2646d9b87314ca46be7672253fbe2
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-11-21 22:12:07 +0300

    Some new methods!

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#TODO усложнить программу, сдлеать ввод данных с клавиатуры
 from item import Item
 from shop import Shop
 
 
 def main():
-    mouse = Item('steelseries', 6, 20, 'wireless')
-    keyboard = Item('logitech', 4, 20, 'bluetooth')
-    monitor = Item('asus')
+    mouse = Item(input('Введите имя товара "1": '), int(input('Введите количество товаров: ')), int(input('Введите стоимость товаров: ')), input('Введите категорию товара: '))
+    keyboard = Item(input('Введите имя товара "2": '), int(input('Введите количество товаров: ')), int(input('Введите стоимость товаров: ')), input('Введите категорию товара: '))
+    monitor = Item(input('Введите имя товара "3": '), int(input('Введите количество товаров: ')), int(input('Введите стоимость товаров: ')), input('Введите категорию товара: '))
     shop = Shop(mouse, keyboard, monitor, Item('printer'))
     s = shop.list_items('было')
     print(shop.sum_of_items('было'))
@@ -16,8 +15,8 @@ def main():
     print(shop.sum_of_items('стало'))
     print(shop.list_items('перед закупкой'))
     print(shop.sum_of_items('перед закупкой'))
-    shop.buy('ffdf', 3)
-    shop.buy('logitech', 3)
+    shop.buy(keyboard, 3)
+    shop.buy(mouse, 3)
     print(shop.list_items('после закупки'))
     print(shop.sum_of_items('после закупки'))
 
